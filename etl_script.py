161fba535e886a82b2c0a4141520459a96cdaf5e
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-09-15 15:04:46 -0400

    feat: scanning all of September 2024 days (1-30)
    feat: scanning all of September 2024 days (1-30)

--- etl_script.py
+++ etl_script.py
@@ -41,7 +41,8 @@ if __name__ == '__main__':
     # This will attempt to run for the next month
     # monarch_etl(target_year, target_month)
 
-    monarch_butterfly_module.monarch_etl_multi_day_scan(2024, 11, 1, 30, conn_string)
+    monarch_butterfly_module.monarch_etl_multi_day_scan(2024, 9, 1, 30, conn_string)
+    # monarch_butterfly_module.monarch_etl_multi_day_scan(2024, 11, 1, 30, conn_string)
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2024, 12, 1, 31, conn_string)
 
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2025, 1, 1, 31, conn_string)
