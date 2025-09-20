3f0b96fb9af6b7cec30f6c65921e99f761343d5c
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-09-20 11:30:44 -0400

    feat: scanning May 2024

--- etl_script.py
+++ etl_script.py
@@ -42,8 +42,8 @@ if __name__ == '__main__':
     # monarch_etl(target_year, target_month)
 
 
-    # monarch_butterfly_module.monarch_etl_multi_day_scan(2024, 5, 1, 31, conn_string)
-    monarch_butterfly_module.monarch_etl_multi_day_scan(2024, 6, 1, 30, conn_string)
+    monarch_butterfly_module.monarch_etl_multi_day_scan(2024, 5, 1, 31, conn_string)
+    # monarch_butterfly_module.monarch_etl_multi_day_scan(2024, 6, 1, 30, conn_string)
 
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2024, 7, 1, 31, conn_string)
 
