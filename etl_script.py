8009ba894786428a1c06482428a995bbab57b86e
Conrad Hansen-Quartey <Chansenq@gmail.com>
2026-01-11 03:31:36 -0500

    nov 2021 scan

--- etl_script.py
+++ etl_script.py
@@ -73,6 +73,7 @@ if __name__ == '__main__':
     # conn_string = conn_string_gcp_docker
     # This will attempt to run for the next month
     # monarch_etl(target_year, target_month)
+    
 
     monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 11, 1, 30, conn_string)
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 12, 1, 31, conn_string)
