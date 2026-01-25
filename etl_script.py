59fdc10ed346ea60c99a51e4bc81e9ee7143553c
Conrad Hansen-Quartey <Chansenq@gmail.com>
2026-01-25 00:10:41 -0500

    feat: May 2021 scan

--- etl_script.py
+++ etl_script.py
@@ -77,15 +77,15 @@ if __name__ == '__main__':
 
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 6, 17, 19, conn_string)
 
-    monarch_butterfly_module.monarch_etl_day_scan(2021, 10, 4, conn_string)
-    monarch_butterfly_module.monarch_etl_day_scan(2021, 10, 25, conn_string)
+    # monarch_butterfly_module.monarch_etl_day_scan(2021, 10, 4, conn_string)
+    # monarch_butterfly_module.monarch_etl_day_scan(2021, 10, 25, conn_string)
 
 
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 1, 1, 31, conn_string)
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 2, 1, 28, conn_string)
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 3, 1, 31, conn_string)
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 4, 1, 30, conn_string)
-    # monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 5, 1, 31, conn_string)
+    monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 5, 1, 31, conn_string)
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 6, 1, 30, conn_string)
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 7, 1, 31, conn_string)
     # monarch_butterfly_module.monarch_etl_multi_day_scan(2021, 8, 1, 31, conn_string)
