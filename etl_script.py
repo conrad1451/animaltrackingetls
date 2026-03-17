6709de28e70148142ddfcd4ac172959e6450e39e
Conrad Hansen-Quartey <Chansenq@gmail.com>
2026-03-16 23:43:55 -0400

    chore: run Sep 2025 scan

--- etl_script.py
+++ etl_script.py
@@ -103,11 +103,10 @@ if __name__ == '__main__':
     # monarch_etl_multi_day_scan(2025, 5, 1, 31, conn_string)
     # monarch_etl_multi_day_scan(2025, 6, 1, 30, conn_string)
     # monarch_etl_multi_day_scan(2025, 7, 1, 31, conn_string)
-    # monarch_etl_multi_day_scan(2025, 8, 1, 31, conn_string)
-    # monarch_etl_multi_day_scan(2025, 9, 1, 30, conn_string)
-    # monarch_etl_multi_day_scan(2025, 10, 1, 31, conn_string)
- 
-    monarch_etl(2025, 10, conn_string)
+    # monarch_etl_multi_day_scan(2025, 8, 1, 31, conn_string) 
+    
+    monarch_etl(2025, 9, conn_string)
+    # monarch_etl(2025, 10, conn_string)
     # monarch_etl(2025, 11, conn_string)
     # monarch_etl(2025, 12, conn_string)
 
