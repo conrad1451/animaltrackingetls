29fc91f1b331dd447b1d675bb1d41ff00a57a095
Conrad Hansen-Quartey <Chansenq@gmail.com>
2026-04-25 02:33:49 -0400

    chore: re-run Mar 2026 scan with correct name for records

--- etl_script.py
+++ etl_script.py
@@ -96,8 +96,9 @@ if __name__ == '__main__':
     # monarch_etl_multi_day_scan(2024, 11, 1, 30, conn_string)
     # monarch_etl_multi_day_scan(2024, 12, 1, 31, conn_string)
       
-      
-    monarch_etl(2024, 12, conn_string)
+
+    # monarch_etl(2024, 11, conn_string)      
+    # monarch_etl(2024, 12, conn_string)
 
     # monarch_etl(2025, 1, conn_string)
     # monarch_etl(2025, 2, conn_string)
