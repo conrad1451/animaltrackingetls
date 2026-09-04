fe7f71f97768ea02afbcb50827bb1718eaa68da2
Conrad Hansen-Quartey <Chansenq@gmail.com>
2026-09-04 11:50:56 -0400

    chore: add Oct 2018 to table tracking

--- table_tracking.py
+++ table_tracking.py
@@ -73,8 +73,8 @@ if __name__ == '__main__':
     # retroactive_table_log.monarch_etl_table_backfill(2018, 7, conn_string)
     # retroactive_table_log.monarch_etl_table_backfill(2018, 8, conn_string)
     # retroactive_table_log.monarch_etl_table_backfill(2018, 9, conn_string)
-    # retroactive_table_log.monarch_etl_table_backfill(2018, 10, conn_string)
-    retroactive_table_log.monarch_etl_table_backfill(2018, 11, conn_string)
+    retroactive_table_log.monarch_etl_table_backfill(2018, 10, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2018, 11, conn_string)
     # retroactive_table_log.monarch_etl_table_backfill(2018, 12, conn_string)
     
     # retroactive_table_log.monarch_etl_table_backfill(2019, 1, conn_string)
