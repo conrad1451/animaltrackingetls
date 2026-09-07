166885cb0688208e454712f1ddf0e84c5b41ed3e
Conrad Hansen-Quartey <Chansenq@gmail.com>
2026-09-06 23:29:11 -0400

    chore: add Jan 2017 to table tracking

--- table_tracking.py
+++ table_tracking.py
@@ -64,8 +64,23 @@ if __name__ == '__main__':
     # monarch_etl(target_year, target_month)
  
 
+
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 1, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 2, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 3, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 4, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 5, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 6, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 7, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 8, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 9, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 10, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 11, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2016, 12, conn_string)
+
+
     # retroactive_table_log.monarch_etl_table_backfill(2017, 1, conn_string)
-    retroactive_table_log.monarch_etl_table_backfill(2017, 2, conn_string)
+    # retroactive_table_log.monarch_etl_table_backfill(2017, 2, conn_string)
     # retroactive_table_log.monarch_etl_table_backfill(2017, 3, conn_string)
     # retroactive_table_log.monarch_etl_table_backfill(2017, 4, conn_string)
     # retroactive_table_log.monarch_etl_table_backfill(2017, 5, conn_string)
