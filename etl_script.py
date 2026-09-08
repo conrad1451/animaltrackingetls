04ad56537b84d81a7d401bcede0fa66d28bb6e1e
Conrad Hansen-Quartey <Chansenq@gmail.com>
2026-09-07 21:22:31 -0400

    chore: run Sep 2015 scan

--- etl_script.py
+++ etl_script.py
@@ -25,8 +25,8 @@ if __name__ == '__main__':
     # monarch_etl(2015, 6, conn_string)   
     # monarch_etl(2015, 7, conn_string)   
     # monarch_etl(2015, 8, conn_string)   
-    # monarch_etl(2015, 9, conn_string)   
-    monarch_etl(2015, 10, conn_string)   
+    monarch_etl(2015, 9, conn_string)   
+    # monarch_etl(2015, 10, conn_string)   
     # monarch_etl(2015, 11, conn_string)   
     # monarch_etl(2015, 12, conn_string) 
     
